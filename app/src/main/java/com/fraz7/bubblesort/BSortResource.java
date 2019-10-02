9873bbc7a017cc9eaa597804162f207f466c160d
fraz78634 <fraz78634@gmail.com>
2019-10-02 15:35:28 +0500

    new

---
+++ app/src/main/java/com/fraz7/bubblesort/BSortResource.java
@@ -0,0 +1,35 @@
+package com.fraz7.bubblesort;
+
+public class BSortResource {
+    private int resourceID;
+    private int description;
+
+    public int getResourceID() {
+        return resourceID;
+    }
+
+    public void setResourceID(int resourceID) {
+        this.resourceID = resourceID;
+    }
+
+    public int getDescription() {
+        return description;
+    }
+
+    public void setDescription(int description) {
+        this.description = description;
+    }
+
+    public BSortResource(int resourceID, int description) {
+        this.resourceID = resourceID;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "BSortResource{" +
+                "resourceID=" + resourceID +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
